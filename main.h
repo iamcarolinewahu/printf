cba0b536070734e8e25b0bd2a5cc82927572315d
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-19 07:01:47 -0700

    Printf main header

--- main.h
+++ main.h
@@ -2,31 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
-
-/**
- * struct format- match the conversion specifiers for printf
- *
- * @id: type char pointer of specifier
- * @f: pointer to function for the conversion specifier
- */
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} change_match;
 
 int _printf(const char *format, ...);
-int print_char(va_list val);
-int print_string(va_list val);
-int print_percent(void);
-int _putchar(char c);
-int _strlen(char *s);
-int _strlenc(const char *s);
-
 
 #endif
