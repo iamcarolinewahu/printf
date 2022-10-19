339ee7d9f0a72f692a9e9a5d2a5ed5d6817cfe55
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-19 06:33:25 -0700

    Creating the printf function

--- _printf.c
+++ _printf.c
@@ -10,36 +10,11 @@
 
 int _printf(const char *format, ...)
 {
-	change_match m[] = {
-		{"%c", print_char},  {"%s", print_string}, {"%%", print_percent}
-	};
+	int res;
 
-	int i = 0, j, len = 0;
 	va_list args;
 
 	va_start(args, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-loop:
-	while (format[1] != '\0')
-	{
-		j = 3;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto loop;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
+	return (res);
 }
