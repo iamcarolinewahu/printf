ce56563193795e58176c4fe4c1e282f6d99a8d9f
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-19 07:27:37 -0700

    Printf function creation

--- main.h
+++ main.h
@@ -2,30 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
-
-/**
- * struct format- match the conversion specifiers for printf
- *
- * @id: type char pointer of specifier
- * @f: pointer to function for the conversion specifier
- */
-
-typedef struct format
-{
-	char *type;
-	int (*f)();
-} fmt_t;
 
 int _printf(const char *format, ...);
-int print_op(const char *format, fmt_t *print_arr, va_list list);
-int print_char(va_list val);
-int print_string(va_list val);
-int print_percent(void);
-int _putchar(char c);
-int _strlen(char *s);
-int _strlenc(const char *s);
 
+#endif
